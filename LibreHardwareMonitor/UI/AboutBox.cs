9631852b81a7901a949e38a7c76225b62bf4d94d
wlkmanist <vlad.king5555@mail.ru>
2024-08-07 21:29:18 +0300

    0.9.3-unofficial-240807
    Signed-off-by: wlkmanist <vlad.king5555@mail.ru>

--- LibreHardwareMonitor/UI/AboutBox.cs
+++ LibreHardwareMonitor/UI/AboutBox.cs
@@ -4,8 +4,12 @@
 // Partial Copyright (C) Michael Möller <mmoeller@openhardwaremonitor.org> and Contributors.
 // All Rights Reserved.
 
+using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using LibreHardwareMonitor.UI.Themes;
 
@@ -19,7 +23,7 @@ public sealed partial class AboutBox : Form
         Font = SystemFonts.MessageBoxFont;
         label3.Text = "Version " + Application.ProductVersion;
         projectLinkLabel.Links.Remove(projectLinkLabel.Links[0]);
-        projectLinkLabel.Links.Add(0, projectLinkLabel.Text.Length, "https://github.com/LibreHardwareMonitor/LibreHardwareMonitor");
+        projectLinkLabel.Links.Add(0, projectLinkLabel.Text.Length, "https://github.com/wlkmanist/LibreHardwareMonitor");
         licenseLinkLabel.Links.Remove(licenseLinkLabel.Links[0]);
         licenseLinkLabel.Links.Add(0, licenseLinkLabel.Text.Length, "https://www.mozilla.org/en-US/MPL/2.0/");
         Theme.Current.Apply(this);
