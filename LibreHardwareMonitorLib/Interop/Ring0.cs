5194ffc39a6a10b39f60a54f98eebabdcd49dcd6
wlkmanist <vlad.king5555@mail.ru>
2024-12-24 01:53:31 +0300

    Ring0: add more IOCTL function codes
    Signed-off-by: wlkmanist <vlad.king5555@mail.ru>

--- LibreHardwareMonitorLib/Interop/Ring0.cs
+++ LibreHardwareMonitorLib/Interop/Ring0.cs
@@ -17,12 +17,21 @@ internal static class Ring0
 
     private const uint OLS_TYPE = 40000;
 
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_GET_DRIVER_VERSION = new(OLS_TYPE, 0x800, Kernel32.IOControlCode.Access.Any);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_GET_REFCOUNT = new(OLS_TYPE, 0x801, Kernel32.IOControlCode.Access.Any);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_MSR = new(OLS_TYPE, 0x821, Kernel32.IOControlCode.Access.Any);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_WRITE_MSR = new(OLS_TYPE, 0x822, Kernel32.IOControlCode.Access.Any);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_PMC = new(OLS_TYPE, 0x823, Kernel32.IOControlCode.Access.Any);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_IO_PORT = new(OLS_TYPE, 0x831, Kernel32.IOControlCode.Access.Read);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_WRITE_IO_PORT = new(OLS_TYPE, 0x832, Kernel32.IOControlCode.Access.Write);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_IO_PORT_BYTE = new(OLS_TYPE, 0x833, Kernel32.IOControlCode.Access.Read);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_IO_PORT_WORD = new(OLS_TYPE, 0x834, Kernel32.IOControlCode.Access.Read);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_IO_PORT_DWORD = new(OLS_TYPE, 0x835, Kernel32.IOControlCode.Access.Read);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_WRITE_IO_PORT_BYTE = new(OLS_TYPE, 0x836, Kernel32.IOControlCode.Access.Write);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_WRITE_IO_PORT_WORD = new(OLS_TYPE, 0x837, Kernel32.IOControlCode.Access.Write);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_WRITE_IO_PORT_DWORD = new(OLS_TYPE, 0x838, Kernel32.IOControlCode.Access.Write);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_PCI_CONFIG = new(OLS_TYPE, 0x851, Kernel32.IOControlCode.Access.Read);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_WRITE_PCI_CONFIG = new(OLS_TYPE, 0x852, Kernel32.IOControlCode.Access.Write);
     public static readonly Kernel32.IOControlCode IOCTL_OLS_READ_MEMORY = new(OLS_TYPE, 0x841, Kernel32.IOControlCode.Access.Read);
+    public static readonly Kernel32.IOControlCode IOCTL_OLS_WRITE_MEMORY = new(OLS_TYPE, 0x842, Kernel32.IOControlCode.Access.Write);
 }
